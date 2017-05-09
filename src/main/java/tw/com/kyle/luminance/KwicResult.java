70fd8fdc2941d1b0e2b6ec2685be3dea8b4a677d
seantyh <seantyh@crl.psy.ntu.edu.tw>
2017-05-09 16:31:11 +0800

    Begin integration with Sketcher

--- src/main/java/tw/com/kyle/luminance/KwicResult.java
+++ src/main/java/tw/com/kyle/luminance/KwicResult.java
@@ -19,12 +19,31 @@ public class KwicResult {
     public List<LumToken> succ_context;
     public List<LumToken> keyword;
 
-    @Override
-    public String toString() {
+    public String toStringRepr(boolean mark_key) {
         String prec = new LumToken.LumTokenListStringBuilder(prec_context).toString();
         String succ = new LumToken.LumTokenListStringBuilder(succ_context).toString();
         String key = new LumToken.LumTokenListStringBuilder(keyword).toString();
-        return String.format("%s\u3000<%s>\u3000%s", prec, key, succ);
+        StringBuilder sb = new StringBuilder();
+        if (!prec.isEmpty()) {
+            sb.append(prec);sb.append("\u3000");
+        }
+        
+        if (mark_key) {
+            sb.append(String.format("<%s>", key));
+        } else {
+            sb.append(String.format("%s", key));
+        }
+        
+        if (!succ.isEmpty()){
+            sb.append("\u3000"); sb.append(succ);
+        }
+        
+        return sb.toString();
+    }
+
+    @Override
+    public String toString() {
+        return toStringRepr(true);
     }
 
     public static class KwicJsonList {
@@ -37,18 +56,18 @@ public class KwicResult {
 
         public JsonArray toJson() {
             JsonArray cr_jarr = new JsonArray();
-            for (KwicResult kwic : kwic_list) {                
+            for (KwicResult kwic : kwic_list) {
                 JsonArray prec_arr = new LumToken.LumTokenListStringBuilder(kwic.prec_context).toJsonArray();
                 JsonArray succ_arr = new LumToken.LumTokenListStringBuilder(kwic.succ_context).toJsonArray();
                 JsonArray cr_arr = new LumToken.LumTokenListStringBuilder(kwic.keyword).toJsonArray();
                 JsonObject cr_obj = new JsonObject();
                 cr_obj.add("prec", prec_arr);
                 cr_obj.add("succ", succ_arr);
-                cr_obj.add("target", cr_arr);    
-                
+                cr_obj.add("target", cr_arr);
+
                 cr_jarr.add(cr_obj);
             }
-            
+
             return cr_jarr;
         }
 
---
+++ src/test/java/tw/com/kyle/luminance/test/SketcherTest.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tw.com.kyle.luminance.test;
+
+import com.google.gson.JsonObject;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import static junit.framework.Assert.assertTrue;
+import static junit.framework.Assert.fail;
+import org.junit.Test;
+import tw.com.kyle.luminance.Concordance;
+import tw.com.kyle.luminance.KwicResult;
+import tw.com.kyle.luminance.LumReader;
+import tw.com.kyle.luminance.Luminance;
+import tw.com.kyle.sketcher.Sketcher;
+
+/**
+ *
+ * @author Sean_S325
+ */
+public class SketcherTest {
+
+    private String INDEX_DIR = "h:/index_dir";
+    private String rules = "";
+    private void setup() {
+        try {
+            Luminance.clean_index(INDEX_DIR);
+            Luminance lum = new Luminance(INDEX_DIR);
+            String txt = String.join("\n",
+                    Files.readAllLines(Paths.get("etc/test/simple_text.txt"), StandardCharsets.UTF_8));
+            rules = String.join("\n",
+                    Files.readAllLines(Paths.get("etc/test/sketcher_rules.txt"), StandardCharsets.UTF_8));
+            JsonObject elem = (JsonObject) lum.add_document(txt);
+            lum.close();
+        } catch (IOException ex) {
+            System.out.println(ex);
+            fail(ex.toString());
+        }
+    }
+
+    @Test
+    public void testConcordance_Word() throws IOException {
+        setup();
+        try (LumReader reader = new LumReader(INDEX_DIR);) {
+            Concordance concord = new Concordance(reader);
+            List<KwicResult> kwic_list = concord.findWord("詞");            
+            for(KwicResult kr: kwic_list){
+                String input = kr.toStringRepr(false);
+                Sketcher sketcher = new Sketcher();
+                String results = sketcher.match(input, rules);
+                System.out.println(results);
+            }            
+        }
+    }
+}
